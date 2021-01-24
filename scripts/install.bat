2439c30a28d8c3a566bce54aad6e197e10dcfe51
Tester86 <tester86t@gmail.com>
2021-01-24 21:25:01 +0100

    Uploading updated apk

--- scripts/install.bat
+++ scripts/install.bat
@@ -1,3 +1,2 @@
-cd C:\Users\germa\OneDrive\Escritorio\github_projects\sni-app\android\app\build\outputs\apk\debug
+cd C:\Users\germa\OneDrive\Escritorio\German\github_projects\sni-app\android\app\build\outputs\apk\debug
 adb install app-debug.apk
-echo ^G
